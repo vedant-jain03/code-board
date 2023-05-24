ce10357de10fc782862732377dd6f02bc8666f43
vedant-jain03 <vedantjainben10@gmail.com>
2023-05-24 09:24:38 +0530

    feat: added github action 2

--- .github/workflows/scripts/add-welcome-description-in-pr.js
+++ .github/workflows/scripts/add-welcome-description-in-pr.js
@@ -25,8 +25,6 @@ const owner = process.argv[2];
 const repo = process.argv[3];
 const PRnumber = process.argv[4];
 
-if(process.argv[5] === "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") {
-  console.log("hurrrrrray")
-  const comment = "Thankyou for adding PR, this is just testing comment, so you can go now else!";
-  addCommentToPR(owner, repo, PRnumber, comment);
-}
+console.log("hurrrrrray")
+const comment = "Thankyou for adding PR, this is just testing comment, so you can go now else!";
+addCommentToPR(owner, repo, PRnumber, comment);
